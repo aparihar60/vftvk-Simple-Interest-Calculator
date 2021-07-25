24b2211c6d66c5884734ff8a5a1ca6bf472a13ad
aparihar60 <aparihar60@gmail.com>
2021-07-25 11:04:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+   
     var principal = document.getElementById("principal").value;
         var rate = document.getElementById("rate").value;
         var years = document.getElementById("years").value;
@@ -11,5 +11,4 @@ function updateRate()
 {
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
- document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-}   
+} 
