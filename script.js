06484c85a47db06c3a3e75f0d8fdd14e3ecbedaa
aparihar60 <aparihar60@gmail.com>
2021-07-25 11:36:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function compute()
 {
-   
+   alert("Hello! I am an alert box!!");
     var principal = document.getElementById("principal").value;
         var rate = document.getElementById("rate").value;
         var years = document.getElementById("years").value;
