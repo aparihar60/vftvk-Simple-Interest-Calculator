384b3bee4cde196d4b2ff30a44d81719a39cd28b
aparihar60 <aparihar60@gmail.com>
2021-07-25 11:49:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ function compute()
         var interest = principal * years * rate /100;
         var year = new Date().getFullYear()+parseInt(years);
    var amount= principal+interest;
+   if (principal >= 0)
+{
+alert("Please a positive number");
+return false;
+}
+   
+   
    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
 function updateRate() 
